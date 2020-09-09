4abaa78909ec87d6f2695641072e6acba3ff90e6
vieiralima33 <vieiralima33@gmail.com>
2020-09-09 16:41:46 -0300

    improving visual and output, handling input errors

--- main.py
+++ main.py
@@ -1,63 +1,62 @@
 import PySimpleGUI as sg
 
-class WindowScreen():
+class Main():
     def __init__(self):
-        sg.change_look_and_feel('DarkGrey4')
-        # Layout
+        sg.theme('Dark2')
+
         layout = [
-            [sg.Text('\n Name', size=(11,0)), sg.Input(size=(18,0), key='name')],
-            [sg.Text('\nWeight[Kg]', size=(11,0)), sg.Input(size=(18,0), key='weight')],
-            [sg.Text('\nHeight[cm]', size=(11,0)), sg.Input(size=(18,0), key='height')],
-            [sg.Text('\n Gender')],
+            [sg.Text('\n Name', size=(11,0)), sg.InputText('', size=(18,0), key='name')],
+            [sg.Text('\nWeight(Kg)', size=(11,0)), sg.Input('', size=(18,0), key='weight')],
+            [sg.Text('\nHeight(cm)', size=(11,0)), sg.Input('', size=(18,0), key='height')],
             [sg.Radio('Male', 'gender', size=(7,0), key='male'), sg.Radio('Female', 'gender', size=(7,0), key='female')],
+            [sg.Text('\n')],             
             [sg.Button('Calculate')],
-            [sg.Output(size=(32,8))]
+            [sg.Output(size=(38,5))],
         ]
-        # Window
+
         self.window = sg.Window('Calculate Your BMI').layout(layout)
 
     def Start(self):
         while True:
-            # Data Extract
             self.button, self.values = self.window.Read()
             name = str(self.values['name'])
-            weight = int(self.values['weight'])
-            height = int(self.values['height'])
+            try:
+                weight = int(self.values['weight'])
+                height = int(self.values['height'])
+            except ValueError:
+                print('\nYou typed invalid characters, \nTry again with numbers for weight and height...')
+                screen.Start()
             gmale = self.values['male']
             gfemale = self.values['female']
             imc = weight * 10000 / height**2
+            print('\n' * 2)
 
             if gmale == True:
                 print ('Your Body Mass Index is: ', round(imc,2))
                 if imc < 20:
-                    print('\nAttention', name, 'You are under weight for your height!')
+                    print('Attention', name, '\nYou are under weight for your height!')
                 elif imc >= 20 and imc < 25:
-                    print('\nCongratulations', name,  'your weight is normal for your height!')
+                    print('Congratulations', name,  '\nYour weight is normal for your height!')
                 elif imc >= 25 and imc < 30:
-                    print('\nAttention', name, 'you are overweight!')
+                    print('Attention', name, '\nYou are overweight!')
                 elif imc >= 30 and imc < 40:
-                    print('\n Be careful', name, 'you have moderate obesity!')
+                    print('Be careful', name, '\nYou have moderate obesity!')
                 elif imc >= 43:
-                    print('\n Seek help', name, 'you have morbid obesity!')
+                    print('Seek help', name, '\nYou have morbid obesity!')
 
             if gfemale == True:
                 print ('Your Body Mass Index is: ', round(imc,2))
                 if imc < 19:
-                    print('\nAttention', name, 'You are under weight for your height!')
+                    print('Attention', name, '\nYou are under weight for your height!')
                 elif imc >= 19 and imc < 24:
-                    print('\nCongratulations', name, 'your weight is normal for your height!')
+                    print('Congratulations', name, '\nYour weight is normal for your height!')
                 elif imc >= 24 and imc < 29:
-                    print('\nAttention', name, 'you are overweight!')
+                    print('Attention', name, '\nYou are overweight!')
                 elif imc >= 29 and imc < 39:
-                    print('\n Be careful', name, 'you have moderate obesity!')
+                    print('Be careful', name, '\nYou have moderate obesity!')
                 elif imc >= 39:
-                    print('\n Seek help', name, 'you have morbid obesity!')
-            try:
-                height
-                weight
-            except ValueError:
-                print('Invalid caractere, try again with numbers..')
-            finally:
-                screen.Start()
-screen = WindowScreen()
+                    print('Seek help', name, '\nYou have morbid obesity!')
+            if gmale == False and gfemale == False:
+                print ('Make sure you have selected your gender\n')
+screen = Main()
 screen.Start()
